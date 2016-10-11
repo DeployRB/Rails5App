a2b1ca59494140ea7ca3756622126bcfa12064c2
Ilya N. Zykin <ilya.zykin@toptal.com>
2016-10-11 13:09:40 +0300

    Add gem 'config'

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'config', '1.3.0'
+gem 'colorize'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module Rails5App
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    require_relative 'configs'
   end
 end
