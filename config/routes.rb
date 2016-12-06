bfe1f38ddd62c07e1f7fb9164e880760c2f3a309
Ilya N. Zykin <zykin-ilya@ya.ru>
2016-12-06 20:23:54 +0300

    fixup! Protozaur 1

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => '/async/tasks'
   mount ActionCable.server => '/app-cable'
 
-  root to: "notes#index"
+  root to: "protozaur#index"
 
   devise_for :users
   resources  :notes
